2eed9fbc73c502d7d5e40f6310a7ba99d55ced7e
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-16 11:57:09 -0400

    makes fetch post request to db

--- nickelodeon_frontend/scripts/script.js
+++ nickelodeon_frontend/scripts/script.js
@@ -1,10 +1,57 @@
-console.log("INEFFABLE!")
-
+const API = "http://localhost:3000"
 const body = document.querySelector("body")
+const form = document.querySelector("form")
 
 fetch("http://localhost:3000/characters")
   .then(res => res.json())
   .then(renderCharacters)
+
+
+//https://vignette.wikia.nocookie.net/peanutstreet/images/b/bc/Eliza-Thornberry.png
+//   form.addEventListener("submit", (e) =>{
+//       e.preventDefault()
+//       var elements = Array.from(form.elements)
+//       elements.forEach(element => {
+//         let element.name = element.value
+//       })
+//   })
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let reqBody = {};
+    Object.keys(form.elements).forEach(key => {
+        let element = form.elements[key];
+        if (element.type !== "submit") {
+            reqBody[element.name] = element.value;
+        }
+    });
+    console.log(reqBody); // Call to function for form submission
+    fetch("http://localhost:3000/characters", {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(reqBody) // body data type must match "Content-Type" header
+        }).then(res => {
+            if(res.status !== 200){
+                throw new TypeError('BAD STATUS CODE!')
+            }
+            const contentType = res.headers.get('content-type')
+            if (!contentType || !contentType.includes('application/json')){
+                throw new TypeError("NOT JSON!!!")
+            }
+            
+            return res.json()
+        })
+        .then(json =>{
+            // renderCharacter(json)
+            console.log(json)})
+        .catch(err => { 
+            console.log(err)
+        })  
+});
+
+
 
 function renderCharacters(characters){
   const charactersList = document.createElement("div")
@@ -23,6 +70,7 @@ function renderCharacter(character){
     <p>Ally: ${character.ally}</p>
     <p>Enemy: ${character.enemy}</p>
     <p>Played by ${character.actor}</p>
+    <p>Show: ${character.show_name}</p>
   `
   const like = document.createElement("p")
   like.innerText = `${character.name} has $0 in likes!`
@@ -42,7 +90,20 @@ function renderCharacter(character){
     .then(res=> res.json())
     .then(() => div.remove())
   })
-  div.append(like, likeButton, deleteButton)
+
+  const editButton = document.createElement("div")
+  editButton.className = "edit character-button"
+  editButton.innerText = `Edit`
+  editButton.addEventListener("click", () => {
+    // div.remove()
+    fetch(`${API}/characters/${character.id}`, {
+      method: "PATCH"
+    })
+    .then(res=> res.json())
+    .then(data => console.log(data,"edit button"));
+  })
+
+  div.append(like, likeButton, editButton, deleteButton)
 
   charactersList.appendChild(div)
 }
